9f3049a205d0cf9b0f60e8407258c76d63b8c95e
yanggqm <289656703@qq.com>
2018-09-13 23:52:54 +0800

    :whale: first commit

--- app.js
+++ app.js
@@ -1,20 +1,18 @@
 import express from 'express';
-import db from './mongodb/db.js';
 import config from 'config-lite';
 import router from './routes/index.js';
 import cookieParser from 'cookie-parser'
 import session from 'express-session';
 import connectMongo from 'connect-mongo';
-import winston from 'winston';
-import expressWinston from 'express-winston';
 import history from 'connect-history-api-fallback';
 import chalk from 'chalk';
 
 // 实例化express
 const app = express();
 
+// app.all('*')可以拦截所有请求 设置跨域
 app.all('*', (req, res, next) => {
-	res.header("Access-Control-Allow-Origin", req.headers.Origin || req.headers.origin || 'https://cangdu.org');
+	res.header("Access-Control-Allow-Origin", req.headers.Origin || req.headers.origin);
 	res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With");
 	res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
   	res.header("Access-Control-Allow-Credentials", true); //可以带cookies
