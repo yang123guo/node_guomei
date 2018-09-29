a003753fdfa28007ef3260f0538e9772c0082a59
yanggqm <289656703@qq.com>
2018-09-29 23:37:50 +0800

    yang

--- middlewares/history.js
+++ middlewares/history.js
@@ -1,9 +1,9 @@
-// 'use strict'
+'use strict'
 
-// export default (req, res, next) => {
-// 	if ((/manage/gi).test(req.url)) {
-// 		res.sendFile(__dirname + '/public/manage/index.html')
-// 	}else{
-// 		res.sendFile(__dirname + '/public/elm/index.html')
-// 	}
-// }
+export default (req, res, next) => {
+	if ((/manage/gi).test(req.url)) {
+		res.sendFile(__dirname + '/public/manage/index.html')
+	}else{
+		res.sendFile(__dirname + '/public/elm/index.html')
+	}
+}
